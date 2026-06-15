ee258fd66eebc6721fc4aa80ae4a227615af94e8
Subamaarleshar B <66205301+maarlesh@users.noreply.github.com>
2026-06-15 21:41:54 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,61 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.empty() || s.length() == 1){
+            return false;
+        }
+        bool result = false;
+        char openChars[] = {'{', '[' , '('};
+        char closedChars[] = {'}', ']' , ')'};
+        stack<char> st;
+        for(int i=0; i < s.length(); i++){
+            if(isContains(openChars, s[i], 3)){
+                st.push(s[i]);
+            }else if(!st.empty()){
+                if(equivalent(st.top(), s[i])){
+                    st.pop();
+                }else{
+                    cout<<"Returning eearly";
+                    return false;
+                }
+            }else{
+                return false;
+            }
+            // stack<char> temp(st);
+            // while (!temp.empty())
+            // {
+            //     cout << temp.top() << " ";
+            //     temp.pop();
+            // }
+        }
+        if(st.empty()){
+            return true;
+        }else{
+            cout<<"Returning here";
+            return false;
+        }
+    }
+
+    bool isContains(char chars[], char target, int size){
+        for (int i = 0; i < size ; i++) {
+            if (chars[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool equivalent(char s, char c){
+        if(s == '{'){
+            return c == '}';
+        }else if(s == '['){
+            return c == ']';
+        }else if(s == '('){
+            return c == ')';
+        }
+        return false;
+    }
+};
